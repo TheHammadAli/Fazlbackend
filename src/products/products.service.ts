d713829cc808e732cd3418a8a42d670826ef0819
babarkhan32 <babar.0khan1@gmail.com>
2025-09-14 19:25:55 +0200

    removed the condition

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -60,7 +60,7 @@ export class ProductsService {
           throw new BadRequestException('Shop location is missing');
         }
 
-       
+
         productPayload.shopId = shop._id as Types.ObjectId;
         location = shop.location;
         console.log("product payload", productPayload)
