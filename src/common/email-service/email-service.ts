9b712da244ef4999084edca4a548b7a1e710e7f5
babarkhan32 <babar.0khan1@gmail.com>
2026-07-29 18:29:14 +0200

    nodemailer

--- src/common/email-service/email-service.ts
+++ src/common/email-service/email-service.ts
@@ -1,45 +1,35 @@
-import { promises as dns } from 'node:dns';
 import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class EmailService implements OnModuleInit {
-    private readonly logger = new Logger(EmailService.name);
-    private transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: Number(process.env.SMTP_PORT),
-        secure: false,
-        requireTLS: true,
-        family: 4,
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS,
-        },
-        tls: {
-            rejectUnauthorized: true,
-        },
+  private readonly logger = new Logger(EmailService.name);
+
+  private transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: process.env.SMTP_SECURE === 'true',
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+
+  async onModuleInit() {
+    try {
+      await this.transporter.verify();
+      this.logger.log('SMTP connection verified.');
+    } catch (error) {
+      this.logger.error('Failed to verify SMTP connection.', error);
+    }
+  }
+
+  async sendEmail(to: string, subject: string, html: string) {
+    await this.transporter.sendMail({
+      from: process.env.EMAIL_FROM,
+      to,
+      subject,
+      html,
     });
-
-    async onModuleInit() {
-        try {
-            const records = await dns.lookup(process.env.SMTP_HOST!, {
-                all: true,
-            });
-
-            console.log(records);
-            await this.transporter.verify();
-            this.logger.log('SMTP connection verified.');
-        } catch (error) {
-            this.logger.error('Failed to verify SMTP connection.', error);
-        }
-    }
-
-    async sendEmail(to: string, subject: string, html: string) {
-        await this.transporter.sendMail({
-            from: process.env.EMAIL_FROM,
-            to,
-            subject,
-            html,
-        });
-    }
+  }
 }
